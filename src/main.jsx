08f73c619a17717fcbb76b631c7ab23abb5359cd
mananpanchal843-coder <mananpanchal843@gmail.com>
2026-08-22 20:21:40 +0530

    Fix ThemeContext import path for production build

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from './ThemeContext';
 import './index.css';
 import './styles/mobile.css';
 import App from './App.jsx';
